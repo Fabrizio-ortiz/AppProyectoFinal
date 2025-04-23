5fa21c718ca79b86284f43b5d7c36bf9bf1ba2eb
Fabrizio-ortiz <fabrizioortizgarcia@gmail.com>
2025-04-23 18:10:08 -0500

    Se agrego mejoras al menu principal, login y se añadío el activity de registro

--- app/src/main/java/com/example/appproyectofinal/LoginActivity.kt
+++ app/src/main/java/com/example/appproyectofinal/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.appproyectofinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,19 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registroText = findViewById<TextView>(R.id.registro_text)
+
+        registroText.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+        val botonRegresar = findViewById<ImageView>(R.id.icono_regresar)
+
+        botonRegresar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed() // Para regresar a la pantalla anterior
         }
     }
 }
--- app/src/main/java/com/example/appproyectofinal/RegistroActivity.kt
+++ app/src/main/java/com/example/appproyectofinal/RegistroActivity.kt
@@ -1,38 +1,27 @@
 package com.example.appproyectofinal
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-        }
-        val button = findViewById<Button>(R.id.custom_button)
-
-        button.setOnClickListener{
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
         }
 
-        val registroText = findViewById<TextView>(R.id.registro_text)
+        val botonRegresar = findViewById<ImageView>(R.id.icono_regresar)
 
-        registroText.setOnClickListener {
-            val intent = Intent(this, RegistroActivity::class.java)
-            startActivity(intent)
+        botonRegresar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed() // Para regresar a la pantalla anterior
         }
-
     }
 }
